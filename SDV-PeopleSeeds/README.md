56d7cc6994fb3f2e9b785aaad25e74fa3c6dc000
Thalzius <thalzius@gmail.com>
2024-12-05 12:53:10 -0700

    Combined All of bwdy mods
    Combined bwdy mods into one mod compilation.

---
+++ SDV-PeopleSeeds/README.md
@@ -0,0 +1,2 @@
+# SDV-PeopleSeeds
+A Stardew Valley mod for... well, it is exactly what it sounds like.
---
+++ SDV-Polygamy/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+[assembly: AssemblyTitle("Polygamy")]
+[assembly: AssemblyDescription("A Stardew Valley mod for those with more love to give.")]
+[assembly: AssemblyCompany("bwdy")]
+[assembly: AssemblyProduct("Polygamy")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyCopyright("Copyright © bwdy 2019")]
